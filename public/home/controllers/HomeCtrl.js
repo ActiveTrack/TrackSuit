f3c49b59a127bf847e0fc1c9ff036cdc1276e9e5
George <georgejweiler@gmail.com>
2016-09-01 08:55:35 -0700

    [client: init/style] set up initial angular module, style home page with images

---
+++ public/home/controllers/HomeCtrl.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+angular.module('ctrlsModule', [])
+  .controller("homeCtrl", function(){
+    //home controller code
+})
--- public/index.js
+++ public/index.js
@@ -1,1 +1,22 @@
 //angular entrypoint
+
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import './home/controllers/HomeCtrl.js'
+
+angular.module('app', [
+  uiRouter, 
+  'ctrlsModule'
+  ])
+  .config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/home')
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: './home/templates/home.html',
+        controller: 'homeCtrl',
+        controllerAs: 'home'
+      })
+  });
+
+
